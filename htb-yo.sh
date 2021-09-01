dbb665b16f4c20649d05dbc52f81e85960d1bbc7
-k <slowdive@me.com>
2021-08-31 22:17:33 -0700

    Update yo.sh

---
+++ htb-yo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cat << "EOF"
+___ ___ _______
+|   Y   |   _   |
+|   1   |.  |   |
+\_   _/|.  |   |
+ |:  | |:  1   |
+ |::.| |::.. . |
+ `---' `-------'
+EOF
+read -p "target name:"  name
+read -p "target IP:"  IP
+echo "target set to $IP!"
+mkdir -p $name/recon
+cd $name
+printf "\033[41myo $name!\033[0m \n"
+nmap -sC -sV -oN recon/initial_nmap.txt -e tun0 -v -Pn $IP
+sudo masscan -p1-65535 -e tun0 -oL recon/allports.txt --rate=1000 -vv -Pn $IP
+gobuster dir -u http://$IP -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o recon/dirscan.txt
+/opt/ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$IP/FUZZ | tee recon/ffuf.txt
