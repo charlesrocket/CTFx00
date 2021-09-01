288dc0a23608651bcb096a2f807385eda2dcd346
-k <slowdive@me.com>
2021-09-01 10:31:51 -0700

    Update htb-yo.sh

--- htb-yo.sh
+++ htb-yo.sh
@@ -17,4 +17,4 @@ printf "\033[41myo $name!\033[0m \n"
 nmap -sC -sV -oN recon/initial_nmap.txt -e tun0 -v -Pn $IP
 sudo masscan -p1-65535 -e tun0 -oL recon/allports.txt --rate=1000 -vv -Pn $IP
 gobuster dir -u http://$IP -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o recon/dirscan.txt
-/opt/ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$IP/FUZZ | tee recon/ffuf.txt
+ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$IP/FUZZ | tee recon/ffuf.txt
